c36b9247759ca8b8b1a5125cc795a3affc610084
llang llang <zzzliyang@gmail.com>
2020-02-23 12:20:01 +0800

    more comments

--- project2.py
+++ project2.py
@@ -32,6 +32,7 @@ def closure(R, F, S):
                 new_f.append(dependency)
     return result
 
+
 ## Determine the all the attribute closure excluding superkeys that are not candidate keys given the schema R and functional dependency F
 def all_closures(R, F):
     # call the recursive worker function with initialized values
@@ -56,88 +57,34 @@ def all_closures(R, F):
             result = [subset, closure_result]
             all_result.append(result)
     return all_result
-    
+
+
 ## Return a minimal cover of the functional dependencies of a given schema R and functional dependencies F.
 def min_cover(R, FD):
     new_fd = min_cover_step1(R, FD)
     return min_cover_step2(R, new_fd)
 
-## Return all minimal covers reachable from the functional dependencies of a given schema R and functional dependencies F.
-def min_covers_step1(R, FD):
-    new_fd = []
-    # simplify right hand side
-    for dependency in FD:
-        left = dependency[0]
-        right = dependency[1]
-        for item in right:
-            new_fd.append([left, [item]])
-    closures = all_closures(R, FD)
-    new_fd = [item for item in new_fd if not is_subset(item[0], item[1])]
-    choices = []
-    static_items = []
-    for fd_item in new_fd:
-        choice = []
-        fd_left = fd_item[0]
-        for closure in closures:
-            closure_left = closure[0]
-            if len(closure_left) >= len(fd_item[0]):
-                break
-            if is_subset(closure[1], fd_item[1]) and is_subset(fd_item[0], closure_left):
-                fd_left = closure_left
-                to_add_choice = True
-                for existing_choice in choice:
-                    if is_subset(closure_left, existing_choice[0]):
-                        to_add_choice = False
-                        break
-                if to_add_choice:
-                    choice.append([closure_left, fd_item[1]])
-        if len(choice) > 1:
-            is_new_choice = True
-            for choice_item in choices:
-                if set_equals(choice_item, choice):
-                    is_new_choice = False
-                    break
-            if is_new_choice:
-                choices.append(choice)
-        else:
-            static_items.append([fd_left, fd_item[1]])
-    all_combi = []
-    get_all_combi([], choices, all_combi)
-    result = []
-    for combi in all_combi:
-        temp_fd = remove_fd_dup(combi + static_items)
-        is_new = True
-        for existing in result:
-            if fd_equals(temp_fd, existing):
-                is_new = False
-                break
-        if is_new:
-            result.append(temp_fd)
-    return result
-
-def remove_fd_dup(fd):
-    result = []
-    for i in range(len(fd)):
-        found = False
-        for j in range(i + 1, len(fd)):
-            if fd_equals([fd[i]], [fd[j]]):
-                found = True
-                break
-        # only keep the fd if it's not found in the later part of this list
-        if not found:
-            result.append(fd[i])
-    return result
-
-def get_all_combi(current_list, remain_list, result):
-    if len(remain_list) == 0:
-        result.append(current_list)
-        return
-    for i in range(len(remain_list[0])):
-        get_all_combi(current_list + [remain_list[0][i]], remain_list[1:], result)
 
 def min_covers(R, FD):
     '''
-    Explain the rationale of the algorithm here.
+    Call min_covers_step1 and get new_fds after
+      1. simplify RHS
+      2. Find all possible ways to simplify LHS and keep all combinations
+          eg. if ['A', 'B', 'C'] can be simplified by both ['A', 'B'] and ['B', 'C'],
+          then we need to keep both possibilities as different FD sets to be considered further
+    From each new_fd in new_fds, we try to find a min_cover by
+      1. find all redundant FDs and put into fd_redundant, ie it can be inferred from other FDs so safe to remove
+      2. find all FDs that has to be present in the final result and put into fd_to_keep
+      3. find all subset of fd_redundant, as each of it can be a candidate in the final min cover
+      4. for each of the candidate subset sub, check whether sub + fd_to_keep is a min cover by
+         a. any of its FD cannot be removed
+         b. its all closures is the same as the original all closures
+      5. since each sub is unique, we can simply add sub + fd_to_keep to the final result if it pass the above criteria
+    Note: I am not using the algorithm mentioned in notes to consider all different orders and
+             try to remove redundant FDs one by one because it has a complexity of n!
+          That algorithm works find when we only want any one of the min covers(what I implemented in min_cover)
+          By checking all possible redundant subsets, the complexity is capped at 2^n, which is much better than n!.
+          When redundant FDs are not many it's even better.
     '''
     results = []
     new_fds = min_covers_step1(R, FD)
@@ -149,7 +96,7 @@ def min_covers(R, FD):
             redundant = False
             left = new_fd[i][0]
             right = new_fd[i][1]
-            new_closures = all_closures(R, new_fd[:i] + new_fd[i+1:])
+            new_closures = all_closures(R, new_fd[:i] + new_fd[i + 1:])
             for new_closure in new_closures:
                 if set_equals(new_closure[0], left):
                     if is_subset(new_closure[1], right):
@@ -165,10 +112,13 @@ def min_covers(R, FD):
             else:
                 fd_to_keep.append(new_fd[i])
         fd_redundant_sub = []
+        # find all subsets of all redundant FDs
         get_all_subset([], fd_redundant, fd_redundant_sub)
+        # for each subset, check whether it is a min cover
         for sub in fd_redundant_sub:
             temp_result = sub + fd_to_keep
             is_minimal = True
+            # check whether all FDs in this set are not redundant
             for i in range(len(temp_result)):
                 left = temp_result[i][0]
                 right = temp_result[i][1]
@@ -183,62 +133,40 @@ def min_covers(R, FD):
                     if len(new_closure[0]) > len(left):
                         is_minimal = False
                         break
+            # check whether all closures of this set is same as original
             if is_minimal:
                 closures_to_check = all_closures(R, temp_result)
                 if fd_equals(closures_to_check, full_closures):
                     results.append(temp_result)
     return results
 
-def get_all_subset(current_list, remain_list, result):
-    if len(remain_list) == 0:
-        result.append(current_list)
-        return
-    get_all_subset(current_list + [remain_list[0]], remain_list[1:], result)
-    get_all_subset(current_list.copy(), remain_list[1:], result)
-
-def fd_equals(fd1_list, fd2_list):
-    if len(fd1_list) != len(fd2_list):
-        return False
-    for fd1 in fd1_list:
-        found = False
-        left1 = fd1[0]
-        right1 = fd1[1]
-        for fd2 in fd2_list:
-            left2 = fd2[0]
-            right2 = fd2[1]
-            if set_equals(left1, left2) and set_equals(right1, right2):
-                found = True
-        if not found:
-            return False
-    return True
-
-def get_all_perm(current_list, remain_list, result):
-    if len(remain_list) == 0:
-        result.append(current_list)
-        return
-    for i in range(len(remain_list)):
-        get_all_perm(current_list + [remain_list[i]], remain_list[:i] + remain_list[i+1:], result)
 
 ## Return all minimal covers of a given schema R and functional dependencies F.
 def all_min_covers(R, FD):
     '''
-    Explain the rationale of the algorithm here.
+    find all closures "closures" of the original set of FD
+    start from "closures" to find all of its min covers
     '''
     closures = all_closures(R, FD)
     return min_covers(R, closures)
 
+
 ## You can add additional functions below
+# helper function to check whether 2 sets(list) are equals
 def set_equals(set1, set2):
     return len(set1) == len(set2) and is_subset(set1, set2)
 
+
+# helper function to check whether small sets(list) is a subset of big
 def is_subset(big, small):
     for item in small:
         if item not in big:
             return False
     return True
 
+
+# recursive function to find all subset of certain length
 def get_all_set_rec(remaining_list, length, current_list, result_list, super_key_list):
-    # recursive function to find all subset of certain length
     # furthermore the subset should not be a supreset of any superkey
     if len(super_key_list) > 0:
         for super_key in super_key_list:
@@ -262,6 +190,7 @@ def get_all_set_rec(remaining_list, length, current_list, result_list, super_key
     get_all_set_rec(remaining_list_copy, length, current_list_copy, result_list, super_key_list)
     # branch with 1st item in the remaining_list NOT added to current_list
     get_all_set_rec(remaining_list_copy, length, current_list.copy(), result_list, super_key_list)
+
 
 def min_cover_step1(R, FD):
     new_fd = []
@@ -291,6 +220,7 @@ def min_cover_step1(R, FD):
                 fd_item[0] = closure_left
                 break
     return new_fd
+
 
 def min_cover_step2(R, new_fd):
     # flag whether to continue
@@ -325,6 +255,122 @@ def min_cover_step2(R, new_fd):
                 break
     return new_fd
 
+
+# helper function in finding min_covers, detailed explaination in min_cover comments
+def min_covers_step1(R, FD):
+    new_fd = []
+    # simplify right hand side
+    for dependency in FD:
+        left = dependency[0]
+        right = dependency[1]
+        for item in right:
+            new_fd.append([left, [item]])
+    closures = all_closures(R, FD)
+    # remove trivial FD where RHS is a subset of LHS
+    new_fd = [item for item in new_fd if not is_subset(item[0], item[1])]
+    choices = []
+    static_items = []
+    # find all possibilities to simplify LHS
+    for fd_item in new_fd:
+        choice = []
+        fd_left = fd_item[0]
+        for closure in closures:
+            closure_left = closure[0]
+            if len(closure_left) >= len(fd_item[0]):
+                break
+            # find a possible choice to replace the LHS
+            if is_subset(closure[1], fd_item[1]) and is_subset(fd_item[0], closure_left):
+                fd_left = closure_left
+                to_add_choice = True
+                # check whether this choice is already added
+                for existing_choice in choice:
+                    if is_subset(closure_left, existing_choice[0]):
+                        to_add_choice = False
+                        break
+                if to_add_choice:
+                    choice.append([closure_left, fd_item[1]])
+        # only add the found choice into choices list if
+        #  1. it has more than 1 items
+        #  2. it is not already in choices
+        if len(choice) > 1:
+            is_new_choice = True
+            for choice_item in choices:
+                if set_equals(choice_item, choice):
+                    is_new_choice = False
+                    break
+            if is_new_choice:
+                choices.append(choice)
+        # otherwise add the current item as it cannot be simplify or it only has 1 way to simplify
+        else:
+            static_items.append([fd_left, fd_item[1]])
+    all_combi = []
+    # get all possible comibations if any of the FD can be simplified in multiple ways
+    get_all_combi([], choices, all_combi)
+    result = []
+    # reomve all the duplicates and only keep unique results
+    for combi in all_combi:
+        temp_fd = remove_fd_dup(combi + static_items)
+        is_new = True
+        for existing in result:
+            if fd_equals(temp_fd, existing):
+                is_new = False
+                break
+        if is_new:
+            result.append(temp_fd)
+    return result
+
+
+# helper function to remove duplicates inside a set of FD
+def remove_fd_dup(fd):
+    result = []
+    for i in range(len(fd)):
+        found = False
+        for j in range(i + 1, len(fd)):
+            if fd_equals([fd[i]], [fd[j]]):
+                found = True
+                break
+        # only keep the fd if it's not found in the later part of this list
+        if not found:
+            result.append(fd[i])
+    return result
+
+
+# helper function to find all combinations of a list of items
+def get_all_combi(current_list, remain_list, result):
+    if len(remain_list) == 0:
+        result.append(current_list)
+        return
+    for i in range(len(remain_list[0])):
+        get_all_combi(current_list + [remain_list[0][i]], remain_list[1:], result)
+
+
+# helper function to find all subset of a set(list) of items
+def get_all_subset(current_list, remain_list, result):
+    if len(remain_list) == 0:
+        result.append(current_list)
+        return
+    get_all_subset(current_list + [remain_list[0]], remain_list[1:], result)
+    get_all_subset(current_list.copy(), remain_list[1:], result)
+
+
+# helper function to check whether 2 sets of FD are equal
+def fd_equals(fd1_list, fd2_list):
+    if len(fd1_list) != len(fd2_list):
+        return False
+    for fd1 in fd1_list:
+        found = False
+        left1 = fd1[0]
+        right1 = fd1[1]
+        for fd2 in fd2_list:
+            left2 = fd2[0]
+            right2 = fd2[1]
+            if set_equals(left1, left2) and set_equals(right1, right2):
+                found = True
+        if not found:
+            return False
+    return True
+
+
 ## Main functions
 def main():
     ### Test case from the project
@@ -338,14 +384,14 @@ def main():
     print(all_closures(R, FD2))
 
     R = ['A', 'B', 'C', 'D', 'E', 'F']
-    FD = [[['A'], ['B', 'C']],[['B'], ['C','D']], [['D'], ['B']],[['A','B','E'], ['F']]]
+    FD = [[['A'], ['B', 'C']], [['B'], ['C', 'D']], [['D'], ['B']], [['A', 'B', 'E'], ['F']]]
     R2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    FD2 = [[['A'], ['B']],[['A', 'B', 'C','D'], ['E']], [['E', 'F'], ['G', 'H']],[['A', 'C', 'D','F'], ['E', 'G']]]
+    FD2 = [[['A'], ['B']], [['A', 'B', 'C', 'D'], ['E']], [['E', 'F'], ['G', 'H']], [['A', 'C', 'D', 'F'], ['E', 'G']]]
     print(min_cover(R, FD))
     print(min_cover(R2, FD2))
 
     R = ['A', 'B', 'C']
-    FD = [[['A', 'B'], ['C']],[['A'], ['B']], [['B'], ['A']]] 
+    FD = [[['A', 'B'], ['C']], [['A'], ['B']], [['B'], ['A']]]
     print(min_covers(R, FD))
     print(all_min_covers(R, FD))
 
@@ -353,7 +399,8 @@ def main():
 
     ## Tutorial questions
     R = ['A', 'B', 'C', 'D', 'E']
-    FD = [[['A', 'B'],['C']], [['D'],['D', 'B']], [['B'],['E']], [['E'],['D']], [['A', 'B', 'D'],['A', 'B', 'C', 'D']]]
+    FD = [[['A', 'B'], ['C']], [['D'], ['D', 'B']], [['B'], ['E']], [['E'], ['D']],
+          [['A', 'B', 'D'], ['A', 'B', 'C', 'D']]]
     R2 = ['A', 'B', 'C']
     FD2 = [[['A'], ['B']], [['B'], ['C']], [['C'], ['A']]]
 
@@ -362,8 +409,6 @@ def main():
     print(all_min_covers(R, FD))
     print(all_min_covers(R2, FD2))
 
+
 if __name__ == '__main__':
     main()
-
-
-
